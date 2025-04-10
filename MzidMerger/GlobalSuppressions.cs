8c48cac9a2d565e60db679fe17d555bf0e8de607
alchemistmatt <matthew.monroe@pnnl.gov>
2025-04-09 17:34:22 -0700

    Suppress warnings

---
+++ MzidMerger/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:MzidMerger.MzidMerging.Merge(PSI_Interface.IdentData.IdentDataObjs.IdentDataList{PSI_Interface.IdentData.IdentDataObjs.SpectrumIdentificationResultObj},PSI_Interface.IdentData.IdentDataObjs.IdentDataList{PSI_Interface.IdentData.IdentDataObjs.SpectrumIdentificationResultObj},System.Double,System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:MzidMerger.MzidMerging.ReadAndPreprocessFile(System.String,MzidMerger.Options)~PSI_Interface.IdentData.IdentDataObj")]
